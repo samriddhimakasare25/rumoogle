05a772e8b6c0c55d863399575022712d45720f06
rumezaa <rumeza06@gmail.com>
2025-01-07 23:45:20 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,10 @@
-// app/layout.js
 "use client";
 import Header from "@/components/Header";
 import "./globals.css"; // Your global styles
 import { Roboto, Ropa_Sans } from "next/font/google";
 import Footer from "@/components/Footer";
 import Mailer from "@/components/Mailer";
-import { useState } from "react";
+import { useState, Suspense } from "react";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -24,12 +23,12 @@ const ropaSans = Ropa_Sans({
 export default function RootLayout({ children }) {
   const [isExpand, setIsExpand] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <html lang="en" className={`${ropaSans.variable} bg-dark-purple-100`}>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
         <link rel="icon" href="/icon.ico" type="image/x-icon" sizes="200x256" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <title>rumoogle</title>
@@ -43,28 +42,38 @@ export default function RootLayout({ children }) {
         <meta name="twitter:image" content="/Banner.png" />
       </head>
       <body className="flex flex-col min-h-screen relative">
-        <Header setShowMailer={setIsOpen} />
+        <Suspense fallback={<div>Loading Header...</div>}>
+          <Header setShowMailer={setIsOpen} />
+        </Suspense>
 
         <main className="flex-grow flex">
-          <div className="w-full">{children}</div>
-        </main>
-        {isOpen && (
-          <div
-            className={`${
-              isExpand
-                ? "fixed inset-0 bg-gray-900 bg-opacity-70 z-40 flex items-center justify-center"
-                : "fixed inset-0 bg-gray-900 bg-opacity-7 flex items-center justify-center md:block  md:bg-transparent md:inset-auto md:bottom-0 md:right-0 shadow-2xl"
-            } `}
-            style={{ zIndex: 100 }}
-          >
-            <Mailer
-              isExpand={isExpand}
-              isOpen={isOpen}
-              setIsExpand={setIsExpand}
-              setIsOpen={setIsOpen}
-            />
+          <div className="w-full">
+            <Suspense fallback={<div>Loading Body...</div>}>
+              {children}
+            </Suspense>
           </div>
+        </main>
+
+        {isOpen && (
+          <Suspense fallback={<div>Loading Mailer...</div>}>
+            <div
+              className={`${
+                isExpand
+                  ? "fixed inset-0 bg-gray-900 bg-opacity-70 z-40 flex items-center justify-center"
+                  : "fixed inset-0 bg-gray-900 bg-opacity-7 flex items-center justify-center md:block  md:bg-transparent md:inset-auto md:bottom-0 md:right-0 shadow-2xl"
+              } `}
+              style={{ zIndex: 100 }}
+            >
+              <Mailer
+                isExpand={isExpand}
+                isOpen={isOpen}
+                setIsExpand={setIsExpand}
+                setIsOpen={setIsOpen}
+              />
+            </div>
+          </Suspense>
         )}
+
         <Footer />
       </body>
     </html>
